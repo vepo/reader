bbada6d092ab3673904bb6e4b249dd8a09bd44ba
Victor Osório <victor.perticarrari@gmail.com>
2025-01-08 14:39:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 angular.module('readerApp', [])
   .controller('ViewController', ['$subscriptions', function($subscriptions) {
-    console.log('Starting View Controller', $subscriptions);
+    console.log('Starting View Controller', $subscriptions, $subscriptions.call());
   }])
   .controller('SubscriptionsController', ['$subscriptions', function($subscriptions) {
-    console.log('Starting Subscriptions Controller', $subscriptions);    
+    console.log('Starting Subscriptions Controller', $subscriptions, $subscriptions.call());    
+    
   }])
   .factory('$subscriptions', ['$http', function($http) {
     console.log('Starting Subscriptions Service', $http);
+    return {
+      call: function() {
+        console.log("Calling");
+        return "yeah";
+      }
+    };
   }]);
